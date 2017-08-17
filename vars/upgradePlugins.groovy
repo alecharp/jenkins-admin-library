1dfc8a788c4dd849d3abbe38a155ea3a80e6ea2d
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2017-08-17 17:15:51 +0200

    Use default variable in iterators
    Thanks to script-security 1.33

--- vars/upgradePlugins.groovy
+++ vars/upgradePlugins.groovy
@@ -2,13 +2,13 @@
 
 def call() {
   def plugins = jenkins.model.Jenkins.instance.pluginManager.activePlugins
-    .findAll { pl -> pl.hasUpdate() }
-    .collect { pl -> pl.getShortName() }
+    .findAll { it.hasUpdate() }
+    .collect { it.getShortName() }
   println "Plugins to upgrade: ${plugins.join(', ')}"
   long count = 0
 
-  jenkins.model.Jenkins.instance.pluginManager.install(plugins, false).each { f ->
-    f.get()
+  jenkins.model.Jenkins.instance.pluginManager.install(plugins, false).each {
+    it.get()
     println "${++count}/${plugins.size()}.."
   }
 
